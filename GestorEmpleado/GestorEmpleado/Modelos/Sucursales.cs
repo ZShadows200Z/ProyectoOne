cd17b9e18cd4f5da098a102b89ef9a3f6cc85031
Winterbox400 <sandigojustin@gmail.com>
2026-07-02 20:06:01 -0600

    Creación Entidades en C# dentro de Carpeta Modelos

---
+++ GestorEmpleado/GestorEmpleado/Modelos/Sucursales.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorEmpleado.Modelos
+{
+    public class Sucursales
+    {
+        public int IdSucursal { get; set; }
+        public string Nombre { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ GestorEmpleado/GestorEmpleado/Modelos/Usuarios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorEmpleado.Modelos
+{
+    public class Usuarios
+    {
+        public int IdUsusario { get; set; }
+        public string Nombre { get; set; }
+        public string Usuario { get; set; }
+        public byte[] Contraseña { get; set; }
+        public bool Activo { get; set; }
+    }
+}
