5e22b0c382116a80370ae35fec78ab955dbc2480
Winterbox400 <sandigojustin@gmail.com>
2026-07-02 18:47:52 -0600

    Query - Creacion de tablas BD

---
+++ QuerysBaseDeDatos/CreacionTablas.sql
@@ -0,0 +1,60 @@
+--Aquí Creamos la base de datos
+CREATE DATABASE DB_GestorEmpleados
+GO
+
+--Corremos el comando usar para poder trabajar dentro de la base de datos creada
+USE DB_GestorEmpleados
+GO
+
+
+--Creamos todas las tablas igual al diagrama relacional hecho en Drawio
+CREATE TABLE Cargos(
+IdCargo INT PRIMARY KEY IDENTITY,--Esto hace que se cree un registro secuencial
+Nombre VARCHAR(100) NOT NULL,
+Activo BIT NOT NULL DEFAULT 1 --Lo inicializamos en 1, significa que esta activo
+)
+GO
+
+CREATE TABLE TipoContrataciones(
+IdTipoContratacion INT PRIMARY KEY IDENTITY,
+Tipo VARCHAR(50) NOT NULL,
+Activo BIT NOT NULL DEFAULT 1
+)
+GO
+
+CREATE TABLE Sucursales(
+IdSucursal INT PRIMARY KEY IDENTITY,
+Nombre VARCHAR(50) NOT NULL,
+Activo BIT NOT NULL DEFAULT 1
+)
+GO
+
+CREATE TABLE MotivoExoneraciones(
+IdMotivoExoneracion INT PRIMARY KEY IDENTITY,
+Motivo VARCHAR(50) NOT NULL,
+Activo BIT NOT NULL DEFAULT 1
+)
+GO
+
+CREATE TABLE Empleados(
+IdEmpleado INT PRIMARY KEY IDENTITY,
+Nombre VARCHAR(200) NOT NULL,
+Identificacion VARCHAR(20) NOT NULL UNIQUE,
+FechaIngreso DATETIME NOT NULL,
+FechaExoneracion DATETIME NOT NULL,
+IdMotivoExoneracion INT FOREIGN KEY REFERENCES MotivoExoneraciones(IdMotivoExoneracion),--Esta es la manera de conectar la tabla Empleados con las tabla MotivoExoneraiones atravez del ID
+IdTipoContratacion INT FOREIGN KEY REFERENCES TipoContrataciones(IdTipoContratacion),
+IdSucural INT FOREIGN KEY REFERENCES Sucursales(IdSucursal),
+IdCargo INT FOREIGN KEY REFERENCES Cargos(IdCargo),
+Activo BIT NOT NULL DEFAULT 1
+)
+GO
+
+CREATE TABLE Usuarios(
+IdUsuario INT PRIMARY KEY IDENTITY,
+Nombre VARCHAR(200) NOT NULL,
+Usuario VARCHAR(50) NOT NULL UNIQUE,
+Contraseña VARBINARY(MAX) NOT NULL, --Es así y no INT o VARCHAR porque con una función en codigo utilizando SHA512 se genera una contraseña unica
+Activo BIT NOT NULL DEFAULT 1
+)
+GO
